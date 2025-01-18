bc936b0b16b84ec382d0939abbc431abd84b81f3
Juan Cruz <jclopezponce@axter.com.ar>
2025-01-18 18:32:22 +0100

    fix images

--- src/projects.js
+++ src/projects.js
@@ -1,30 +1,29 @@
 
-
 const projects = [{
     name : "project1",
     description :"lorefwewe",
     link : "likfe",
-    image : "./public/test-img.jpg"
+    image : "./public/img/test-img.jpg"
   }, {
     name : "project2",
     description :"este es tu projecto",
     link : "likfe",
-    image : "./public/test-img.jpg"
+    image : "./public/img/test-img.jpg"
   }, {
     name : "project3",
     description :"otro proy",
     link : "likfe",
-    image : "./public/test-img.jpg"
+    image : "./public/img/test-img.jpg"
   }, {
     name : "project3",
     description :"otro proy",
     link : "likfe",
-    image : "./public/test-img.jpg"
+    image : "./public/img/test-img.jpg"
   }, {
     name : "project3",
     description :"otro proy",
     link : "likfe",
-    image : "./public/test-img.jpg"
+    image : "./public/img/test-img.jpg"
   }];
 
   export default projects
